4f21b66ac1326152300739f933254441d5e32c48
VictoremWinbringer <victortriumphator@gmail.com>
2016-11-10 12:49:25 +0300

    Закончил проект

---
+++ SortList/SortList/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortList
+{
+    /// <summary>
+    /// Объектное представление карточки
+    /// </summary>
+   public class Cart
+    {
+        public string Start { get; set; }
+        public string Finish { get; set; }
+
+        public static IEnumerable<Cart> SortCart(Cart[] c)
+        {
+            Array.Sort(c, new Comparer());
+            return c;
+        }
+    }
+
+    /// <summary>
+    /// Переопределения класса для сравнения объектов в массиве.
+    /// </summary>
+   public class Comparer : IComparer<Cart>
+    {
+        public int Compare(Cart x, Cart y)
+        {
+            if (string.Compare(x.Finish, y.Start, true) == 0)
+                return -1;
+            if (string.Compare(x.Start, y.Finish, true) == 0)
+                return 1;
+            return 0;
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cart[] m = new Cart[]
+            {
+                new Cart {Start ="Мельбурн", Finish="Кельн" },
+                new Cart { Start="Москва" , Finish="Париж" },
+                 new Cart { Start="Кельн" , Finish="Москва" }
+            };
+            foreach (var i in m)
+            {
+                Console.WriteLine($"{i.Start} -> {i.Finish}");
+            }
+            Console.WriteLine(new string('-', 30));
+            var c = Cart.SortCart(m);
+            foreach (var i in c)
+            {
+                Console.WriteLine($"{i.Start} -> {i.Finish}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SortList/UnitTestProject1/Cart.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SortList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class CartTest
+    {
+        [TestMethod]
+        public void Sort()
+        {
+            // Arrange
+            Cart[] m = new Cart[]
+            {
+                new Cart {Start ="Мельбурн", Finish="Кельн" },
+                new Cart { Start="Москва" , Finish="Париж" },
+                 new Cart { Start="Кельн" , Finish="Москва" }
+            };
+
+            // Act
+            var c = Cart.SortCart(m);
+
+            // Assert
+            Assert.IsNotNull(c);
+            Assert.IsTrue( isEqual( c.ToArray(), new Cart[]
+            {
+                new Cart {Start ="Мельбурн", Finish="Кельн" },
+
+                 new Cart { Start="Кельн" , Finish="Москва" },
+                new Cart { Start="Москва" , Finish="Париж" }
+            }));
+
+            //Assert.AreEqual(c.ToArray(), new Cart[]
+            //{
+            //    new Cart {Start ="Мельбурн", Finish="Кельн" },
+
+            //     new Cart { Start="Кельн" , Finish="Москва" },
+            //    new Cart { Start="Москва" , Finish="Париж" }
+            //});
+        }
+        /// <summary>
+        /// Метод для проверки равенства двух массивов
+        /// </summary>
+        /// <param name="x"> Левый массив для сравнения</param>
+        /// <param name="y">Правый массив для сравнения</param>
+        /// <returns> true если массивы равны false если они не равны</returns>
+        public bool isEqual(Cart[] x, Cart[] y)
+        {
+            if (x.Length != y.Length)
+                return false;
+            if (x.Zip(y, (l, r) => l.Finish.Equals(r.Finish, StringComparison.InvariantCultureIgnoreCase)
+              && l.Start.Equals(r.Start, StringComparison.InvariantCultureIgnoreCase))
+              .All(z => z == true))
+                return true;
+            return false;
+        }
+    }
+}
