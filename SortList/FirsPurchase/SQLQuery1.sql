96e4a725717970c8081867e4c201b34c76afb230
VictoremWinbringer <victortriumphator@gmail.com>
2016-11-11 17:42:58 +0300

    Внес правки в SQL запросс

--- SortList/FirsPurchase/SQLQuery1.sql
+++ SortList/FirsPurchase/SQLQuery1.sql
@@ -1,10 +1,9 @@
 ﻿
-SELECT ProductId, COUNT(*) as CountFirstPurchase
+SELECT ProductId, COUNT(*) as firstPurchaseCount
 FROM(
   SELECT ProductId, CustomerId, DateCreated, 
-    MIN(DateCreated) OVER(PARTITION BY   CustomerId) as MinDate
+    MIN(DateCreated) OVER(PARTITION BY   CustomerId) as minDate
   FROM Sales
 )as T
-WHERE DateCreated = MinDate
+WHERE DateCreated = minDate
 GROUP BY ProductId
-ORDER BY COUNT(*) 
